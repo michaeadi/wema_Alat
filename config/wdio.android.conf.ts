f169822097050012cccc3fc63418c14c8d976867
Michael Adinebo <michaeladinebo@Michaels-MacBook-Pro.local>
2024-08-20 18:01:35 +0100

    initial commit

---
+++ config/wdio.android.conf.ts
@@ -0,0 +1,45 @@
+import { join } from 'node:path';
+import { config as baseConfig } from './wdio.shared.local.conf.ts';
+
+export const config: WebdriverIO.Config = {
+    ...baseConfig,
+
+    // ============
+    // Specs
+    // ============
+    specs: [
+        '../tests/specs/*.spec.ts',
+    ],
+
+    // ============
+    // Capabilities
+    // ============
+    // For all capabilities please check
+    // https://github.com/appium/appium-uiautomator2-driver
+    capabilities: [
+        {
+            // The defaults you need to have in your config
+            platformName: 'Android',
+            // maxInstances: 1,
+            // For W3C the appium capabilities need to have an extension prefix
+            // This is `appium:` for all Appium Capabilities which can be found here
+
+            //
+            // NOTE: Change this name according to the Emulator you have created on your local machine
+            'appium:deviceName': 'Pixel_7_Pro_Android_14_API_34',
+            //
+            // NOTE: Change this version according to the Emulator you have created on your local machine
+            'appium:platformVersion': '14.0',
+            'appium:orientation': 'PORTRAIT',
+            'appium:automationName': 'UiAutomator2',
+            // The path to the app
+            'appium:app': join(
+                process.cwd(),
+                'apps',
+                './apps/QA_Dev_test_test_build.apk'
+            ),
+            'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
+            'appium:newCommandTimeout': 240,
+        },
+    ],
+};
